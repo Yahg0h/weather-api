5494ee34753ecb5f0af702a980e7b30be319b552
Yahg0h <yagoviniciusandradedias208@gmail.com>
2026-07-06 23:09:18 -0300

    feat: Add POST /register endpoint

--- main.py
+++ main.py
@@ -69,3 +69,40 @@ class City(BaseModel):
 # Function for Open-Meteo API climate searching
 def build_weather_url(lat: float, lon: float):
     return f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+
+@app.post("/register")
+def register(user: UserAuth):
+    # Get user inputs (request info)
+    username = user.username
+    password = user.password
+
+    # Hash password before inserting into the database
+    hashed_pass = pwd_context.hash(password)
+
+    # Connect to database
+    with engine.connect() as conn:
+        query = conn.execute(text("SELECT * FROM users WHERE username = :username"), {"username": username})
+        existing_user = query.fetchone()
+
+        # If username already exists, return error 400
+        if existing_user is not None:
+            raise HTTPException(status_code=400, detail="Username already exists. Try Again.")
+        
+        # If it doesn't exist, add the new user into the database
+        conn.execute(text("INSERT INTO users (username, password) VALUES (:username, :password)"), {"username": username, "password": hashed_pass})
+        conn.commit()
+
+        # Get id from the recently added user
+        query = conn.execute(text("SELECT * FROM users WHERE id = LAST_INSERT_ID()"))
+        results = query.fetchone()
+
+        # Convert row to dict
+        results_dict = dict(results._mapping)
+
+        # Reorganize the new dict with user_id, and return it
+        new_dict = {
+            "id": results_dict["id"],
+            "message": "User created successfully."
+        }
+        return new_dict
+    
